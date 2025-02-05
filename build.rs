8a87728c280a948c42dbfb8b10a1cf6f8625d3f9
sunyuze <dashuaige991@qq.com>
2025-02-05 10:05:59 +0800

    0

--- build.rs
+++ build.rs
@@ -8,6 +8,9 @@ fn main() {
 
     // 指定要链接的静态库的名字（不带前缀 'lib' 和后缀）
     println!("cargo:rustc-link-lib=static=pq"); // 注意这里应该是 "pq" 而不是 "libpq"
+
+
+    // 加载配置文件，如果有的话
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
     // 加载配置文件，如果有的话
@@ -16,5 +19,5 @@ fn main() {
     // 生成绑定并写入到输出目录下的头文件中
     cbindgen::generate_with_config(crate_dir, config)
         .expect("Unable to generate bindings")
-        .write_to_file(PathBuf::from(env::var("OUT_DIR").unwrap()).join("libtool.h"));
+        .write_to_file(PathBuf::from(env::var("OUT_DIR").unwrap()).join("lib_tool.h"));
 }
